7ce2a91c43f3569808948f66bda1621e30fe87bc
clownami <roccoyt228@gmail.com>
2022-08-08 15:58:58 +0300

    Final 3.0

--- animationApp/Services/DataStorage.swift
+++ animationApp/Services/DataStorage.swift
@@ -11,70 +11,8 @@ import SpringAnimation
 class DataStorage {
     static let shared = DataStorage()
     
-//    let animations = [
-//        "pop",
-//        "slideLeft",
-//        "slideRight",
-//        "slideDown",
-//        "slideUp",
-//        "squeezeLeft",
-//        "squeezeRight",
-//        "squeezeDown",
-//        "squeezeUp",
-//        "fadeIn",
-//        "fadeOut",
-//        "fadeOutIn",
-//        "fadeInLeft",
-//        "fadeInRight",
-//        "fadeInDown",
-//        "fadeInUp",
-//        "zoomIn",
-//        "zoomOut",
-//        "fall",
-//        "shake",
-//        "flipX",
-//        "flipY",
-//        "morph",
-//        "squeeze",
-//        "flash",
-//        "wobble",
-//        "swing"
-//    ]
-    
     let animations: [AnimationPreset] = AnimationPreset.allCases
-    
     let curves: [AnimationCurve] = AnimationCurve.allCases
-//    let curves = [
-//        "easeIn",
-//        "easeOut",
-//        "easeInOut",
-//        "linear",
-//        "spring",
-//        "easeInSine",
-//        "easeOutSine",
-//        "easeInOutSine",
-//        "easeInQuad",
-//        "easeOutQuad",
-//        "easeInOutQuad",
-//        "easeInCubic",
-//        "easeOutCubic",
-//        "easeInOutCubic",
-//        "easeInQuart",
-//        "easeOutQuart",
-//        "easeInOutQuart",
-//        "easeInQuint",
-//        "easeOutQuint",
-//        "easeInOutQuint",
-//        "easeInExpo",
-//        "easeOutExpo",
-//        "easeInOutExpo",
-//        "easeInCirc",
-//        "easeOutCirc",
-//        "easeInOutCirc",
-//        "easeInBack",
-//        "easeOutBack",
-//        "easeInOutBack"
-//    ]
     
     private init() {}
 }
